658fd8b1194a99b8481287c15e6da44a5eb11401
MrSinkaaa <sinkapersinka@gmail.com>
2023-10-13 13:48:10 +0300

    Added functional buttons to the application.

--- src/application/gui/ButtonsPanel.java
+++ src/application/gui/ButtonsPanel.java
@@ -10,60 +10,57 @@ public class ButtonsPanel extends JPanel {
 
     private final JButton startButton = new JButton("start");
     private final JButton pauseButton = new JButton("pause");
-    private final JButton resetSimulationButton = new JButton("reset");
     private final JButton turnNextButton = new JButton("turn next");
-
-    private final Map map;
 
     public ButtonsPanel() {
         this.add(startButton);
         this.add(pauseButton);
-        this.add(resetSimulationButton);
         this.add(turnNextButton);
 
         this.setBackground(Color.LIGHT_GRAY);
-        this.setPreferredSize(new Dimension(300,35));
+        this.setPreferredSize(new Dimension(300, 35));
         this.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
         this.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
 
 
-        this.map = Simulation.getMap();
         turnNextButtonClicked();
         startSimulationButtonClicked();
-  //      resetSimulationButtonClicked();
+        pauseSimulationButtonClicked();
     }
 
     public void turnNextButtonClicked() {
         turnNextButton.addActionListener(e -> {
-            Simulation.getGame().nextTurn(map);
+            Simulation.getGame().nextTurn();
         });
     }
 
     public void startSimulationButtonClicked() {
         startButton.addActionListener(e -> {
-
-//            Thread thread = new Thread(() -> {
-            Simulation.getGame().startGame(map);
-            //            });
-
+            startButton.setEnabled(false);
+            new StartButtonWorker().execute();
         });
     }
 
     public void pauseSimulationButtonClicked() {
-        pauseButton.addActionListener(e -> {
-            Simulation.getGame().stopGame();
+        SwingUtilities.invokeLater(() -> {
+            pauseButton.addActionListener(e -> {
+                Simulation.getGame().stopGame();
+            });
         });
     }
 
-//    public void resetSimulationButtonClicked() {
-//        resetSimulationButton.addActionListener(e -> {
-//            Simulation.getGame().stopGame();
-//            try {
-//                game = new Game();
-//                game.startGame(map);
-//            } catch (InterruptedException ex) {
-//                throw new RuntimeException(ex);
-//            }
-//        });
-//    }
+    private class StartButtonWorker extends SwingWorker<Void, Void> {
+
+        @Override
+        protected Void doInBackground() {
+            Simulation.getGame().startGame();
+            return null;
+        }
+
+        @Override
+        protected void done() {
+            startButton.setEnabled(true);
+        }
+    }
+
 }
